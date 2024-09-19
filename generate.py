7aafae370537fdf6f648b88c1990305c727fd2f2
Maowen-H <kingnk2019@outlook.com>
2024-09-19 17:41:27 -0400

    generate.py

--- generate.py
+++ generate.py
@@ -5,19 +5,25 @@ with open("data_info.txt", 'r') as f:
 
 # print(data_file)
 f.close()
-print(data_file)
-small_list = []
-other_list  = []
+#print(data_file)
+low = []
+other  = []
 for i in range(2,len(data_file)):
-    dim, _, _, _, _, file = data_file[i].split()
+    dim, _, _, _, _, file = data_file[i].split('\t')
     if dim.strip() == 'small':
-        small_list += os.path.join("data", file.strip())
+        low += [os.path.join("data/", file.strip())]
     else:
-        other_list += os.path.join("data", file.strip())
+        other+= [os.path.join("data/", file.strip())]
 
-print(small_list)
+for kind in ["low","other"]:
+    with open(f"SnD_{kind}_dim.sh",'w') as f:
+        f.write(f"mkdir -p tmp/SnD_{kind}_dim\n")
+        f.write(f"rm tmp/SnD_{kind}_dim/*\n")
+    for file in eval(kind):
+        with open(f"SnD_{kind}_dim.sh", 'a') as f:
+            f.write(f"python3.13 SnD.py {file} | tee tmp/SnD_{kind}_dim/{file.split('/')[-1]} &\n")
 
-    
+ 
 
 
 
